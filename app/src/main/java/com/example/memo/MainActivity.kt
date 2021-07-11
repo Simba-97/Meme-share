56577842d273601dba4349046502b8f254da4f41
Hritik Kumar Singh <singhhritik978@gmail.com>
2021-07-11 12:11:14 +0530

    Finished meme app

---
+++ app/src/main/java/com/example/memo/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.memo
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class MainActivity : AppCompatActivity() {
+
+    var url: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadMeme()
+    }
+
+    private fun loadMeme() {
+
+        findViewById<ProgressBar>(R.id.progressBar).visibility = View.VISIBLE
+        val queue = Volley.newRequestQueue(this)
+        url = "https://meme-api.herokuapp.com/gimme"
+//        val targ = findViewById<ImageView>(R.id.memeView)
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                url = response.getString("url")
+                Glide.with(this).load(url).listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
+                        return true
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        findViewById<ProgressBar>(R.id.progressBar).visibility = View.GONE
+                        return false
+                    }
+                }).into(findViewById<ImageView>(R.id.memeView))
+            },
+            { error ->
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+            }
+        )
+        queue.add(jsonObjectRequest)
+
+    }
+
+    fun shareMeme(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey!, Check this cool meme $url")
+        val chooser = Intent.createChooser(intent, "Share this meme using")
+        startActivity(chooser)
+    }
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+}
